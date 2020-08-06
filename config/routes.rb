437bd4449e1b6b1c4be990ce0352a1c282abc47f
sasha370 <kng.sasha@mail.ru>
2020-08-06 16:49:24 +0300

    add Index page & Materialize gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Задаем главный путь, который будет обработан в контроллере course, используя метод index
+  # вьюха будет находится в папке courses/index
+  root 'courses#index'
+
+  # При получении на сервере Get запроса about, перенаправить в контроллер pages, использовать экшн about
+  # Вьюха также будет находится в папке pages/about
+  get "about", to: 'pages#about'
+  get 'courses/new', to: 'courses#new'
 end
