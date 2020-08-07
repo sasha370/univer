bf90e33c606311b91ed72a984cd352184c2af98a
sasha370 <kng.sasha@mail.ru>
2020-08-07 12:12:28 +0300

    add Edit for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,13 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+
+
   def new
     @student = Student.new
   end
@@ -14,11 +21,32 @@ class StudentsController < ApplicationController
       flash[:notice] = "Вы успешно зарегистрировлись"
       redirect_to root_path
     else
-      flash[:error] =  "Что-то пошло не так"
+      flash[:error] =  "У вас есть ошибки"
       render :new
     end
   end
 
+  # Экшен  отрисовыает шаблон для редактированияБ поэтому ему нужны параметры для отрисовки
+    def edit
+    @student = Student.find(params[:id])
+  end
+
+  # Экшн изменяет параметры, поэтому использует POST и params
+  def update
+    # идентичен Create, но сначала ищет в БД по ID и только потом пытается update строку
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "Изменения сохранены"
+      # После отправляем на собственную страницу
+      # согласно роутингу это будет students/id ,
+      # но в сокращенке можно просто писать id
+      #  вместо redirect_to student_path(@student)
+       redirect_to @student
+    else
+      flash[:error] =  "У вас есть ошибки"
+      render :new
+    end
+  end
 
   private
 
