ab1bb776986d4d4f4c99409440a16447d09bdb2c
sasha370 <kng.sasha@mail.ru>
2020-08-07 12:42:55 +0300

    some refactoring

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,15 @@
 class StudentsController < ApplicationController
+  # перед каждым из перечисленных методов запустить метод set_student
+  before_action :set_student, only: [:show, :edit, :update]
+
+
   def index
     @students = Student.all
   end
 
 
   def show
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
   end
 
 
@@ -19,7 +23,7 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params )
     if @student.save
       flash[:notice] = "Вы успешно зарегистрировлись"
-      redirect_to root_path
+      redirect_to @student
     else
       flash[:error] =  "У вас есть ошибки"
       render :new
@@ -28,13 +32,13 @@ class StudentsController < ApplicationController
 
   # Экшен  отрисовыает шаблон для редактированияБ поэтому ему нужны параметры для отрисовки
     def edit
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
   end
 
   # Экшн изменяет параметры, поэтому использует POST и params
   def update
     # идентичен Create, но сначала ищет в БД по ID и только потом пытается update строку
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:notice] = "Изменения сохранены"
       # После отправляем на собственную страницу
@@ -53,4 +57,10 @@ class StudentsController < ApplicationController
   def student_params
     params.require(:student).permit(:name, :email)
   end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+
 end
