22029667b8992bf3a3f6d3c09018b29ee7b84e2b
sasha370 <kng.sasha@mail.ru>
2020-08-08 19:27:59 +0300

    add Admin for Student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_184824) do
+ActiveRecord::Schema.define(version: 2020_08_08_162403) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_184824) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
