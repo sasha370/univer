30db96b6d8b334218d24d4c751af05133a50104e
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 20:16:57 +0300

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,21 +1,14 @@
 class StudentsController < ApplicationController
-  # перед каждым из перечисленных методов запустить метод set_student
+
   before_action :set_student, only: [:show, :edit, :update]
-
-  # crbgyenm проверку входа для данных экшенов
   skip_before_action :require_user, only: [:new, :create]
-
-  # Проверить текущий пользователь - это хозяин карточки?
   before_action :require_same_student, only: [:edit, :update]
-
-
 
   def index
     @students = Student.all
   end
 
   def show
-    # @student = Student.find(params[:id])
   end
 
   def new
@@ -33,21 +26,12 @@ class StudentsController < ApplicationController
     end
   end
 
-  # Экшен  отрисовыает шаблон для редактированияБ поэтому ему нужны параметры для отрисовки
   def edit
-    # @student = Student.find(params[:id])
   end
 
-  # Экшн изменяет параметры, поэтому использует POST и params
   def update
-    # идентичен Create, но сначала ищет в БД по ID и только потом пытается update строку
-    # @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:notice] = "Изменения сохранены"
-      # После отправляем на собственную страницу
-      # согласно роутингу это будет students/id ,
-      # но в сокращенке можно просто писать id
-      #  вместо redirect_to student_path(@student)
        redirect_to @student
     else
       flash[:error] =  "У вас есть ошибки"
@@ -66,13 +50,9 @@ class StudentsController < ApplicationController
   end
 
   def require_same_student
-    # если текущий пользователь != данному студенту
     if current_user != @student
-      # то уведомляем и пересылаем на собственную карточку
       flash[:notice] = " Вы можете редактировать только свой профиль"
       redirect_to student_path(current_user)
     end
   end
-
-
 end
