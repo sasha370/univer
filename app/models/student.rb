ec23123323ae1ae3ee24b25ca63e4af35779342b
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 20:17:52 +0300

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,18 +6,12 @@ class Student < ApplicationRecord
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 
-  # Добавляем секретку для хранения паролей
   has_secure_password
 
-  # Student имеют иного отношений с таблицей Студент,Курс
   has_many :student_courses
-  # Студент имеет множество отношения с Курсами, черезтаблицу Student_course
   has_many :courses, through: :student_courses
 
-  # Проверяем админ ли пользователь? Возвращает true /false
   def is_admin?
     admin
   end
-
-
 end
