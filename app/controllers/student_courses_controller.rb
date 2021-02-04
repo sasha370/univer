8f0f07efd8ecf7836e80e64c87e8001372954c43
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 20:15:58 +0300

    Update student_courses_controller.rb

--- app/controllers/student_courses_controller.rb
+++ app/controllers/student_courses_controller.rb
@@ -1,10 +1,7 @@
 class StudentCoursesController < ApplicationController
 
-
   def create
-    # находим нужный курс по переданному ID
     course_to_add = Course.find(params[:course_id])
-    # Проверяем есть ли этот курс у пользователя?
     unless current_user.courses.include?(course_to_add)
       StudentCourse.create(course: course_to_add, student: current_user)
       flash[:notice] = " Вы успешно записались на курс: #{course_to_add.name}"
